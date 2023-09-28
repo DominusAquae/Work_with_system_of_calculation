efa6fc5419dc001e956fee8aabb0e870037bba1a
DominusAquae <physmatitdmisaychev@gmail.com>
2023-09-29 02:48:35 +0300

    Checked and working adding!

--- objects_of_const_number_system_indicator/class_int_n_system_calculation.py
+++ objects_of_const_number_system_indicator/class_int_n_system_calculation.py
@@ -25,8 +25,10 @@ class int_n_system_calculation():
         # then add zeros to the beginning of the first so that the length becomes the same
         if len_of_first_number > len_of_second_number:
             other.number = "0"*(len_of_first_number - len_of_second_number) + other.number
+            len_of_first_number = len_of_first_number
         else:
             self.number = "0"*(-len_of_first_number + len_of_second_number) + self.number
+            len_of_first_number = len_of_second_number
         
         #create an empty string (in the future - the result of addition) and carry number
         result = ""
@@ -39,25 +41,12 @@ class int_n_system_calculation():
             a = self.alfabet.index(self.number[i])
             b = other.alfabet.index(other.number[i])
 
-            """Этот фрагмент кода будет существовать в таком формате только для выполнения дз
-            Автор не принуждает к насилию глаз и просит подождать следующей операции"""
-
-            stroka_vuvoda = ""
-            stroka_vuvoda += str((a + b + carry_nomber)%self.number_system_indicator)
-            stroka_vuvoda += " пишем, "
-            stroka_vuvoda += str((a + b + carry_nomber)//self.number_system_indicator)
-            stroka_vuvoda += " в уме"
-
-            print(stroka_vuvoda)
-
-            """Конец первого куска вырвиглазного кода"""
-
             # When summing such two decimal numbers and taking the remainder of the resulting number,
             # a decimal number is obtained, which is the lowest digit of the sum of the total digit of the original numbers. 
             # When adding a carry number, we add the highest digit of the previous sum to the resulting value,
             # thus obtaining the final value in the same digit for the result of the entire calculation.
             result += self.alfabet[(a + b + carry_nomber)%self.number_system_indicator]
-            carry_nomber = (a + b)//self.number_system_indicator
+            carry_nomber = (a + b + carry_nomber)//self.number_system_indicator
 
         if carry_nomber != 0:
             result += self.alfabet[carry_nomber]
@@ -67,11 +56,4 @@ class int_n_system_calculation():
         """
         Второй и последний кусок вырвиглазного кода
         """
-        print("ответ:", result)
-
-a = input()
-b = input()
-c = int(input())
-first_n = int_n_system_calculation(a, c)
-second_n = int_n_system_calculation(b, c)
-first_n + second_n    
+        return "ответ:", result

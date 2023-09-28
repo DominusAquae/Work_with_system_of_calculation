1e055227cb246ec793025114829ebe3f7dd88624
DominusAquae <physmatitdmisaychev@gmail.com>
2023-09-29 00:24:17 +0300

    Magic metod len

--- objects_of_const_number_system_indicator/class_int_n_system_calculation.py
+++ objects_of_const_number_system_indicator/class_int_n_system_calculation.py
@@ -1,7 +1,9 @@
 class int_n_system_calculation():
+    
     # boundary of application of the class - positional number system with the last digit z
     # The general alphabet for any valid number system looks like this:
     alfabet_start= "0123456789abcdefghijklmnopqrstuvwxyz"
+    
     def __init__(self, a: str, n: int):
         # String representation of a number:
         self.number = a
@@ -9,4 +11,9 @@ class int_n_system_calculation():
         self.number_system_indicator = n
         # Let's limit the existing alphabet
         self.alfabet = self.alfabet_start[:self.number_system_indicator]
+
+    def __len__(self):
+        return len(self.number)
+    
+    
     
